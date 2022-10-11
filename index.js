667deb0d43fbc0c4fac7d6bcabe4fbd1f985dad9
chisom <christianezeanyaeche@gmail.com>
2022-10-11 11:36:10 +0100

    configured prompt to use promise & readline module

--- index.js
+++ index.js
@@ -1,58 +1,72 @@
-const myGameModule = (()=>{
-    const userName = prompt('please enter your name: ')
+const readline = require('readline')
 
-const user = /\S/.test(userName) ? userName : "Player"
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
+const prompt = (query) => new Promise((resolve) => rl.question(`${query} >>> `, resolve));
+
+// declare Player object
 const player = {
-    level: 1,
-    name: user,
-    isGameOn : true
+        level: 1,
+        name: '',
+        isGameOn : true
+    }
+// promise based random number generator
+function randonNum(range) { 
+     return Promise.resolve(Math.floor(Math.random() * range) + 1)
 }
-function guess(max = 2){
-    const correctNum = randonNum(max)
+
+async function guess(max = 2){
     let isCorrect = false
+    const correctNum = await randonNum(max)
 
     while(!isCorrect){
-        const input = prompt(`please enter your guess between 1 - ${max}`)
+        const input = await prompt(`please enter your guess between 1 - ${max}`)
         const playerGuess = parseInt(input)
+
+        // check for Non Numeric inputs
         if(Number.isNaN( playerGuess) ){
-            if(/exit/i.test(input.trim())){
-                console.log('game over')
+            if(/exit/i.test(input)){
                 return player.isGameOn = false
             }
             continue
         }
+        // check if guess was correct
         if(playerGuess === correctNum){
             isCorrect = true
             player.level = player.level + 1
-            console.log(`${player.name}, you got the number correct, the correct number is ${correctNum}`)
+            console.log(`\n${player.name}, you got the number correct, the correct number is ${correctNum}`)
             return;
         }
         console.log(`${player.name}, Your guess was wrong, please try again...`)
+        }
     }
-}
 
-function randonNum(range) { 
-    return Math.floor(Math.random() * range) + 1
-}
+    // IIFE -> Run immediately on execution
+(async()=>{
+    try {
+        console.log('Enter "exit" in the prompt or use Ctrl + C, to quit\n')
 
-function playGame(){
-    console.log('enter "exit" in the prompt to quit')
-    while( player.isGameOn ) {
-        console.log(`you are playing level ${player.level}`)
-        guess(player.level + 1)
+        // prompt user for input
+        const userName = await prompt('please enter your name: ')
+        const user = /\S/.test(userName) ? userName : "Player"
+
+        // set player name
+        player.name = user
+
+        while( player.isGameOn ) {
+            console.log(`\n${player.name}, you are playing level ${player.level}`)
+            await guess(player.level + 1)
+        }
+       
+        rl.close()
+    } catch (error) {
+        console.error("Unable to prompt", error);
     }
-    alert(`you reached level ${player.level}`)
-
-    // reset player level
-    player.level = 1
-    player.isGameOn = true
-}
-
-return { playGame }
 })()
 
-document.querySelector('button').addEventListener('click', ()=>{
-    myGameModule.playGame()
-})
-
+// 
+rl.on('close', () =>{ 
+    console.log(`\nGame Over\n${player.name}, you reached level ${player.level}`)
+    process.exit(0)
+    }
+);
